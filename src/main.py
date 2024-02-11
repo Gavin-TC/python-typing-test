c1a0411a85d777f37920f73cb29916cbaa656020
Gavin TC <gtclonts@gmail.com>
2024-02-11 18:21:06 -0500

    Finish implementation

--- src/main.py
+++ src/main.py
@@ -2,12 +2,27 @@ import time
 import quote_manager
 
 def main() -> None:
-    quote: str = quote_manager.get_quote()
-    print(quote)
-    user_quote: str = input("\n: ")
-    
-    accuracy = quote_manager.get_accuracy(user_quote, quote)
-    print("Accuracy: " + str(accuracy) + "%")
+    while 1: 
+        print("\nPress [ENTER] when you're ready or [Q] to quit.")
+        answer: str = input()
+
+        if answer.lower() == 'q':
+            break
+
+        start = time.time()
+
+        quote: str = quote_manager.get_quote()
+        print(quote)
+        user_quote: str = input("")
+
+        elapsed = time.time() - start
+        
+        accuracy: int = quote_manager.get_accuracy(user_quote, quote)
+        missed_chars: int = quote_manager.get_missed_chars(user_quote, quote)
+        wpm: int = quote_manager.get_wpm(elapsed, quote, user_quote, missed_chars, accuracy)
+
+        print("\nAccuracy: " + str(accuracy) + "%")
+        print("WPM: ", wpm)
 
 
 if __name__ == "__main__":
--- src/quote_manager.py
+++ src/quote_manager.py
@@ -2,16 +2,27 @@ import random
 
 # "Be the change that you wish to see in the world."
 def get_quote() -> str:
-    quotes = list(open("src/quotes.txt", 'r'))
-    return quotes[random.randint(0, len(quotes))].strip('\n')
+    quotes = list(open("quotes.txt", 'r'))
+    return quotes[random.randint(0, len(quotes)-1)].strip('\n')
 
 
-def get_accuracy(input, original) -> float:
+def get_accuracy(input, original) -> int:
     max_chars = len(original)
     final_string = ""
-
     for x in range(len(input)-1):
         if original[x] == input[x]:
             final_string += input[x]
 
-    return (len(final_string) / max_chars) * 100
+    return int((len(final_string) / max_chars) * 100)
+
+
+def get_missed_chars(input, original) -> int:
+    final_string = ""
+    for x in range(len(input)-1):
+        if original[x] != input[x]:
+            final_string += input[x]
+    return final_string
+
+
+def get_wpm(elapsed_time, input, original, missed_chars, accuracy) -> int:
+    return int((len(input) / 5 - len(missed_chars)) / (elapsed_time / 60))
